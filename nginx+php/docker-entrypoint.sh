57d85f6c6e2aec047a11d2556ab65a70bfd10430
Cloud User <centos@ip-10-148-130-229.ap-southeast-1.compute.internal>
2019-04-10 11:19:01 +0000

    update

---
+++ nginx+php/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ "${PARAMS}" == "nginx" ]]; then
+	ln -sf /dev/stdout /var/log/nginx/access.log
+	ln -sf /dev/stderr /var/log/nginx/error.log
+	chown -R www.www /var/web
+	find /var/web/www -type d -exec chmod 750 {} \;
+	find /var/web/www -not -type d -exec chmod 640 {} \;
+	/usr/local/openresty/nginx/sbin/nginx -g "daemon off;"
+
+fi
+
+if [[ "${PARAMS}" == "php" ]]; then
+	chown -R www.www /var/web
+	find /var/web/www -type d -exec chmod 750 {} \;
+	find /var/web/www -not -type d -exec chmod 640 {} \;
+	/usr/local/php7/sbin/php-fpm -y /usr/local/php7/etc/php-fpm.conf
+fi
+exec "$@"
